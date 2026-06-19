09d8a4e8539baf9834e54986fd56c087b17801fb
slvnnst-debug <slvn.nst@gmail.com>
2026-06-19 11:13:15 +0300

    bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "8829750059:AAFGyELlmVcCHlvaa-_d3ZUOdmE5ygDOK9A"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    web_app = WebAppInfo(url="https://USERNAME.github.io/REPO/")
+
+    keyboard = [
+        [KeyboardButton(text="⏳ Открыть таймер", web_app=web_app)]
+    ]
+
+    markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+    await update.message.reply_text(
+        "Нажми кнопку 👇",
+        reply_markup=markup
+    )
+
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
